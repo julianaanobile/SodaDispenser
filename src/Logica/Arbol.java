d33f1b1cd2ad4b62cd75e6a035e684aa7167ca67
Andres08 <camiloandres4@gmail.com>
2012-12-02 18:13:56 -0500

    update

---
+++ src/Logica/Arbol.java
@@ -0,0 +1,91 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Cesar David Loaiza Q
+ */
+public class Arbol {
+   private Nodo raiz;
+   private int decisionMinimax;
+   private ArrayList arbolito;
+   
+   public Arbol(Nodo raiz)
+   {
+      this.raiz = raiz;
+      arbolito = new ArrayList();
+      
+      
+   }
+   
+   public void crearArbol(Nodo raicita)
+   {
+      Nodo nodo1;
+      Nodo nodo2;
+      Nodo nodo3;
+      Nodo nodo4;
+      if(raiz.getAguaDisponible() == 0)
+      {
+         if(raiz.esMax())
+            raiz.setUtilidad(1);
+         else
+            raiz.setUtilidad(-1);
+      }
+      else
+      {
+         if(raiz.esMax())
+         {
+           if( !raiz.hundioNingunoMax() )
+           {
+             nodo4 = new Nodo(raiz, raiz.getAguaDisponible(), true, !raiz.esMax(), raiz.getProfundidad()+1);
+             arbolito.add(nodo4);
+             crearArbol(nodo4);
+           }
+         }
+         else
+         {
+           if( !raiz.hundioNingunoMin() )
+           {
+             nodo4 = new Nodo(raiz, raiz.getAguaDisponible(), true, !raiz.esMax(), raiz.getProfundidad()+1);
+             arbolito.add(nodo4);
+             crearArbol(nodo4);
+           }
+            
+         }
+
+         nodo1 = new Nodo(raiz, raiz.getAguaDisponible()-1, false, !raiz.esMax(), raiz.getProfundidad()+1);
+         arbolito.add(nodo1);
+         crearArbol(nodo1);
+         
+         if( raiz.getAguaDisponible()-2 >= 0)
+         {
+             nodo2 = new Nodo(raiz, raiz.getAguaDisponible()-2, false, !raiz.esMax(), raiz.getProfundidad()+1);
+             crearArbol(nodo2);
+             arbolito.add(nodo2);
+         }  
+         if( raiz.getAguaDisponible()-3 >= 0)
+         {
+            nodo3 = new Nodo(raiz, raiz.getAguaDisponible()-3, false, !raiz.esMax(), raiz.getProfundidad()+1);
+            crearArbol(nodo3);
+            arbolito.add(nodo3);
+         }
+      }
+   }
+      
+      
+         
+      
+      
+      //Nodo nodo2 = new Nodo
+         
+    
+   public int encontrarMiniMax()
+   {
+      
+   }
+}
---
+++ src/Logica/Nodo.java
@@ -0,0 +1,105 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+/**
+ *
+ * @author Cesar David Loaiza Q
+ */
+public class Nodo {
+   private Nodo padre;
+   private int utilidad;
+   private int aguaDisponible;
+   private boolean btnNingunoMax;
+   private boolean btnNingunoMin;
+   private boolean esMax;
+   private int profundidad;
+   
+
+   public Nodo(Nodo padre, int aguaDisponible, boolean btnNinguno, boolean esMax, int profundidad) {
+      this.padre = padre;
+      this.aguaDisponible = aguaDisponible;
+      //this.btnNinguno = btnNinguno;
+      this.esMax = esMax;
+      if( btnNinguno == false)
+      {
+         if(esMax)
+            btnNingunoMax = padre.btnNingunoMax;
+         else
+            btnNingunoMin = padre.btnNingunoMin;
+      }
+      else
+      {
+         if(esMax)
+            btnNingunoMax = !padre.btnNingunoMax;
+         else
+            btnNingunoMin = !padre.btnNingunoMin;
+         
+      }
+      if(esMax)
+         utilidad = (int) Double.NEGATIVE_INFINITY;
+      else
+         utilidad = (int) Double.POSITIVE_INFINITY;
+      this.profundidad = profundidad;
+         
+      
+   }
+
+   public int getAguaDisponible() {
+      return aguaDisponible;
+   }
+
+   public void setAguaDisponible(int aguaDisponible) {
+      this.aguaDisponible = aguaDisponible;
+   }
+
+   public boolean hundioNingunoMax() {
+      return btnNingunoMax;
+   }
+
+   public void setBtnNingunoMax(boolean btnNingunoMax) {
+      this.btnNingunoMax = btnNingunoMax;
+   }
+
+   public boolean hundioNingunoMin() {
+      return btnNingunoMin;
+   }
+
+   public void setBtnNingunoMin(boolean btnNingunoMin) {
+      this.btnNingunoMin = btnNingunoMin;
+   }
+
+   public boolean esMax() {
+      return esMax;
+   }
+
+   public void setEsMax(boolean esMax) {
+      this.esMax = esMax;
+   }
+
+   
+
+   public Nodo getPadre() {
+      return padre;
+   }
+
+   public void setPadre(Nodo padre) {
+      this.padre = padre;
+   }
+
+   public int getUtilidad() {
+      return utilidad;
+   }
+   public int getProfundidad(){
+       return profundidad;
+   }
+
+   public void setUtilidad(int utilidad) {
+      this.utilidad = utilidad;
+   }
+   
+   
+     
+}
