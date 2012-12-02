898a8f58ee6b8a43d52736b6379d55b25dfc4336
loaiza13 <cesar.loaiza@correounivalle.edu.co>
2012-12-02 13:16:28 -0500

    cesar

---
+++ src/proyecto2/Nodo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto2;
+
+/**
+ *
+ * @author Cesar David Loaiza Q
+ */
+public class Nodo {
+   private Nodo padre;
+   private int utilidad;
+   private int aguaDisponible;
+   private boolean btnNinguno;
+   
+   public expandirse(int opcion)
+   {
+      
+   }
+     
+}
